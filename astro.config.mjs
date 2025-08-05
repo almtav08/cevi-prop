e19a5397af58bcd5f969f5919416785f518c06d1
almtav08 <alexmartinez3107@gmail.com>
2025-08-05 15:49:03 +0200

    first web

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://almtav08.github.io',
+  base: '/cevi-prop/',
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
